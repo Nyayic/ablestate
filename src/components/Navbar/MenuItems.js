dbf76730e4ea23c949f264a13c6105f3b9d30e90
Nyayic <chrosbiefannie@gmail.com>
2020-11-20 15:00:46 +0300

    Created navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Developers',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Employers',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'News',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Get Started',
+        url: '#',
+        cName: 'nav-links-mobile',
+    }
+]
