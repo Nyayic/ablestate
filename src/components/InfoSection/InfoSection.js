70838f4a4c561419e59086931e2ee8774479541f
Nyayic <chrosbiefannie@gmail.com>
2020-12-16 12:34:37 +0300

    added featured deveopers

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -3,15 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import colleages from '../../images/new-employee-removebg.png';
+import DevImage from '../../images/woman-1.jpg';
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck'
 import './InfoSection.css';
 
 function Infosection() {
     return(
         <Container fluid >
-            <Row className="bg-dark">
-                <Col className="intro-col">
+            <Row className="bg-dark intro-row">
+                <Col>
                     <h1 className="header">
                         Get acess to <br/> World-class <br/> Technical Talent 
                     </h1>
@@ -32,9 +35,11 @@ function Infosection() {
 
             </Row>
             <div className="row bg-dark">
-                <h2 className="join-header">
+                <div className="col-md-12 text-center">
+                <p className="join-header">
                     Join Thousands of companies hiring through the Ablestate Platform
-                </h2>
+                </p>
+                </div>
 
                 <div  className="container search mt-4">
                     <Form >
@@ -59,11 +64,49 @@ function Infosection() {
                     <h2 className="featured-devs-header">Featured Developers</h2>
                     <h3 className="featured-devs-intro">Source active engineers fit for your Team</h3>
                 </div>
-            </div>
+                {/* Featured developers start here */}
+                <div className="container">
+                    <div className="row mt-4">
+                    <CardDeck className="col-md-12">
+                        <Card className="col-sm-4 ">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />
+                            <Card.Body>
+                                <Card.Title>Nyayic Fanny</Card.Title>
+                                <Card.Text>Frontend Developer
+                                <button className="view-profile-btn">VIEW PROFILE</button> 
 
-            
-            
-        </Container>
+                                </Card.Text>
+                            </Card.Body>
+                            
+                        </Card>
+                        <Card className="col-sm-4">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            
+                            <Card.Body>
+                                <Card.Title>Mark Ssebuliba</Card.Title>
+                                <Card.Text>Project Manager
+                                <button className="view-profile-btn">VIEW PROFILE</button> 
+
+                                </Card.Text>
+                                
+                            </Card.Body>
+                        </Card>
+
+                        <Card className="col-sm-4">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            <Card.Body>
+                            <Card.Title>David Wampamba</Card.Title>
+                            <Card.Text>Fullstack Developer
+                            <button className="view-profile-btn">VIEW PROFILE</button> 
+
+                            </Card.Text>
+                            </Card.Body> 
+                        </Card>
+                    </CardDeck>
+                </div>
+            </div>
+                
+        </div>
+   
+    </Container>
     );
 }
 export default Infosection;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 
