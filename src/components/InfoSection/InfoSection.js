8f093a6a4e4562d4daf3d330eba48152bcae7c6e
Nyayic <chrosbiefannie@gmail.com>
2020-12-16 20:44:00 +0300

    Added Featured devlopers cards

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import colleages from '../../images/new-employee-removebg.png';
+import JobSearch from '../../images/job-search.jpg';
 import DevImage from '../../images/woman-1.jpg';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
@@ -68,22 +69,26 @@ function Infosection() {
                 <div className="container">
                     <div className="row mt-4">
                     <CardDeck className="col-md-12">
-                        <Card className="col-sm-4 ">
+                        <Card className="col-sm-4 card-items">
                             <img variant="top" src={DevImage} alt="Developer" className="dev-image" />
                             <Card.Body>
-                                <Card.Title>Nyayic Fanny</Card.Title>
-                                <Card.Text>Frontend Developer
+                                <Card.Title className="text-center text-light">
+                                    <b>Nyayic Fanny</b>
+                                </Card.Title>
+                                <Card.Text className="text-center text-light">Frontend Developer <br/><br/>
                                 <button className="view-profile-btn">VIEW PROFILE</button> 
 
                                 </Card.Text>
                             </Card.Body>
                             
                         </Card>
-                        <Card className="col-sm-4">
+                        <Card className="col-sm-4 card-items">
                             <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            
                             <Card.Body>
-                                <Card.Title>Mark Ssebuliba</Card.Title>
-                                <Card.Text>Project Manager
+                                <Card.Title className="text-center text-light">
+                                    <b>Mark Ssebuliba</b>
+                                </Card.Title>
+                                <Card.Text className="text-center text-light"> Project Manager <br/><br/>
                                 <button className="view-profile-btn">VIEW PROFILE</button> 
 
                                 </Card.Text>
@@ -91,10 +96,10 @@ function Infosection() {
                             </Card.Body>
                         </Card>
 
-                        <Card className="col-sm-4">
+                        <Card className="col-sm-4 card-items">
                             <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            <Card.Body>
-                            <Card.Title>David Wampamba</Card.Title>
-                            <Card.Text>Fullstack Developer
+                            <Card.Title className="text-center text-light"><b>David Wampamba</b></Card.Title>
+                            <Card.Text className="text-center text-light">Fullstack Developer <br/><br/>
                             <button className="view-profile-btn">VIEW PROFILE</button> 
 
                             </Card.Text>
@@ -102,11 +107,111 @@ function Infosection() {
                         </Card>
                     </CardDeck>
                 </div>
+
+                {/* Featured developers end here */}
+
+                <div className="col-md-12 text-center">
+                    <button className="all-premium">SEE ALL PREMIUM</button>
+                </div>
+
+                {/* All developers start here */}
+                <div className="container">
+                    <div className="row mt-4">
+                    <CardDeck className="col-md-12">
+                        <Card className="col-sm-4 card-items">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />
+                            <Card.Body>
+                                <Card.Title className="text-center text-light">
+                                    <b>Nyayic Fanny</b>
+                                </Card.Title>
+                                <Card.Text className="text-center text-light">Frontend Developer <br/><br/>
+                                <button className="view-profile-btn">VIEW PROFILE</button> 
+
+                                </Card.Text>
+                            </Card.Body>
+                            
+                        </Card>
+                        <Card className="col-sm-4 card-items">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            
+                            <Card.Body>
+                                <Card.Title className="text-center text-light">
+                                    <b>Mark Ssebuliba</b>
+                                </Card.Title>
+                                <Card.Text className="text-center text-light"> Project Manager <br/><br/>
+                                <button className="view-profile-btn">VIEW PROFILE</button> 
+
+                                </Card.Text>
+                                
+                            </Card.Body>
+                        </Card>
+
+                        <Card className="col-sm-4 card-items">
+                            <img variant="top" src={DevImage} alt="Developer" className="dev-image" />                            <Card.Body>
+                            <Card.Title className="text-center text-light"><b>David Wampamba</b></Card.Title>
+                            <Card.Text className="text-center text-light">Fullstack Developer <br/><br/>
+                            <button className="view-profile-btn">VIEW PROFILE</button> 
+
+                            </Card.Text>
+                            </Card.Body> 
+                        </Card>
+                    </CardDeck>
+                </div>
+                {/* All developers end here */}
+
+                <div className="col-md-12 text-center">
+                    <button className="all-premium">SEE ALL MEMBERS</button>
+                </div>
+
             </div>
+
+            </div>
+            
+            {/* How it works starts here */}
+            <div className="container">
+                <div className="row bg-light">
+                    <div className="col-md-12">
+                        <p className="work-intro text-center">How it works</p>
+                        <h1 className="work-header text-center">Go beyond Traditional Hiring</h1>
+                        <img src={JobSearch} className="job-search-img img-fluid" alt="How it works"/>
+                    </div>
+                </div>
                 
-        </div>
+                <div className="row bg-light">
+                    <div class="col-sm-4 text-center">
+                        <h3><b>Browse</b></h3>
+                        <p>Discover your next hire from our<br/> pool of assertive technical <br/>candidates who are <br/>ready to work.</p>
+                    </div>
+                    <div class="col-sm-4 text-center">
+                        <h3><b>Assess</b></h3>
+                        <p>Evaluate candidates <br/>and determine whether <br/>they  have values <br/>similar to your team.</p>
+                    </div>
+                    <div class="col-sm-4 text-center">
+                        <h3><b>Hire</b></h3>
+                        <p>Seamlessly connect with <br/> your  candidate of choice<br/> and hire right away.<br/>No third party required</p>
+                    </div>
+                </div>
+
+            </div>
+
+            {/* How it works ends here */}
+
+            {/* join premium club starts here */}
+            <div className="container">
+            <div className="row bg-light">
+                <div className="mt-4 premium">
+                    <h2 className="premium-club-header">Join Our Premium Club</h2>
+                    <h3 className="premium-club-intro">Spend less time screening</h3>
+                </div>
+            </div>
+            </div>
+            
+            {/* join premium club ends here */}
+
+            </div>
+
+           
    
-    </Container>
+        </Container>
     );
 }
 export default Infosection;
