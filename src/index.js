a90eddcadc67d984f00e864f284cbff4a7222160
Nyayic <chrosbiefannie@gmail.com>
2020-12-22 08:44:29 +0300

    Added twitter nature community

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App';
-import Routes from './routes/Routes';
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Router>
-      <Routes/>
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
