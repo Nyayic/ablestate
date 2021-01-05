cdbc86592370a879b99b12c5f06a47f00bb6d091
Nyayic <chrosbiefannie@gmail.com>
2021-01-05 09:36:32 +0300

    Added employers and developers sign up forms

--- src/pages/Developers/Developers.js
+++ src/pages/Developers/Developers.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import Screening from '../../images/job-interview-bg.png';
 
 function Developers(){
     return(
-        <div>
-            <h1>Developers</h1>
+        <div className="container-fluid bg-light">
+            <div className="row bg-light">
+                <div className="container mt-4 mb-4 jumbotron bg-white">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h1>GET DISCOVERED WITH YOUR ABLESTATE RESUME</h1>
+                            <p>Get hired right away for the job you deserve</p>
+                        </div>
+                        <div className="col-md-8">
+                            <img src={Screening} className="img-fluid" alt="Interview"/>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="container mt-4 mb-4  bg-white">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1>Please complete your profile</h1>
+                            <div className="form col">
+                                <label for="name">Your Name</label>
+                                <input type="text" className="form-control"/>
+                                <label for="role">Prefered Role</label>
+                                <input type="text" className="form-control"/>
+                                <label for="phone">Phone Number</label>
+                                <input type="text" className="form-control"/>
+                                <label for="location">Location</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">Prefered Photo</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">URL</label>
+                                <input type="link" className="form-control"/>
+                                <label for="skill">Skills</label>
+                                <input type="text" className="form-control"/>
+                                <label for="pitch">Your Pitch</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">Profile Content</label>
+                                <input type="textarea" className="form-control"/>
+                                <input type="button" className="btn btn-primary mt-4 form-control" value="Create profile"/>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+            
         </div>
     )
 }
--- src/pages/Employers/Employers.js
+++ src/pages/Employers/Employers.js
@@ -1,18 +1,61 @@
 import React from 'react';
-
+import Screening from '../../images/job-interview-bg.png';
 
 function Employers(){
     return(
-        <div className="container bg-dark">
-            <div className="row">
-                <h1>Welcome to Ablesate custom pool</h1>
-                <h1>Welcome to Ablesate custom pool</h1><h1>Welcome to Ablesate custom pool</h1>
-                <h1>Welcome to Ablesate custom pool</h1>
-                <h1>Welcome to Ablesate custom pool</h1>
-            </div>
-        </div>
+        <div className="container-fluid bg-light">
+            <div className="row bg-light">
+                <div className="container mt-4 mb-4 jumbotron bg-white">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h1>WELCOME ABLESTATE'S CUSTOM POOL</h1>
+                            <p>VOur talent advisors will
+                            be in touch to discuss
+                            your tech recruiting
+                            needs.Our talent advisors will
+                            be in touch to discuss
+                            your tech recruiting
+                            needs.Our talent advisors will
+                            be in touch to discuss
+                            your tech recruiting
+                            needs.</p>
+                        </div>
+                        <div className="col-md-8">
+                            <img src={Screening} className="img-fluid" alt="Interview"/>
+                        </div>
+                    </div>
 
-    );
+                </div>
+                <div className="container mt-4 mb-4  bg-white">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1>Please complete this short form</h1>
+                            <div className="form col">
+                                <label for="name">Your company Name</label>
+                                <input type="text" className="form-control"/>
+                                <label for="role">choose your next hire(s)</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">Your Name</label>
+                                <input type="text" className="form-control"/>
+                                <label for="phone">Phone Number</label>
+                                <input type="text" className="form-control"/>
+                                <label for="location">Location</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">email address</label>
+                                <input type="text" className="form-control"/>
+                                <label for="name">description</label>
+                                <input type="textarea" className="form-control"/>
+                                <input type="button" className="btn btn-primary mt-4 form-control" value="submit"/>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+    )
 }
 
 export default Employers;
